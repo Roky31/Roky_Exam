aa1f853a2ea9a86ba5f15a6a2bdc1c3f57d85b73
Roky31 <Roky31>
2025-09-27 17:15:10 +0500

    1.0.0

---
+++ database/factories/HotelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\City;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hotel>
+ */
+class HotelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $cityId = City::inRandomOrder()->first();
+        return [
+            'city_id' => $cityId,
+            'name' => fake()->words(3, true),
+        ];
+    }
+}
